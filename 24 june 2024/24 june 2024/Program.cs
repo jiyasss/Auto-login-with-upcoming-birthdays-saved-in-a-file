de7556f82abeb18ffe4f1183e03a9043edd3edc6
jiyasss <157277303+jiyasss@users.noreply.github.com>
2024-06-26 13:23:00 +0530

    Add files via upload

---
+++ 24 june 2024/24 june 2024/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Globalization;
+using System.Diagnostics;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.IO;
+using System.Reflection;
+
+namespace MyProgram
+{
+    public class NumberValidator
+    {
+        public static int number(string num)
+        {
+            int a;
+            while (!int.TryParse(num, out a))
+            {
+                Console.WriteLine("Please Only Enter Number");
+                num = Console.ReadLine();
+            }
+            return a;
+        }
+    }
+    public partial class Programs
+    {
+        static string username;
+        static string password;
+        static bool login = false;
+
+        public static string directoryPath;
+        public static string contentFilePath;
+
+        static void SaveToFile()
+        {
+            string pathTo = Assembly.GetExecutingAssembly().Location;
+            DirectoryInfo Dinfo = new DirectoryInfo(pathTo).Parent.Parent.Parent;
+            directoryPath = Path.Combine(Dinfo.FullName, "output");
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            contentFilePath = Path.Combine(directoryPath, "content.txt");
+
+        }
+        public static void Options()
+        {
+            SaveToFile();
+
+            bool continueprocess = true;
+
+            while (continueprocess)
+            {
+                Console.WriteLine("PRESS 1 : LOGIN AGAIN");
+                Console.WriteLine("PRESS 2 : CREATE NEW USER");
+                Console.WriteLine("PRESS 3 : EXIT");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            RegisterAndLogin();
+                            break;
+                        case 2:
+                            CreateNewUser();
+                            break;
+                        case 3:
+                            continueprocess = false;
+                            Console.WriteLine("Exiting , bye-bye xxx");
+
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+        }
+        static void RegisterAndLogin()
+        {
+            Thread registerThread = new Thread(RegisterUser);
+            Thread loginThread = new Thread(LoginUser);
+
+            registerThread.Start();
+            loginThread.Start();
+
+            registerThread.Join();
+            loginThread.Join();
+
+            if (login)
+            {
+                
+                DisplayWelcome();
+                Birthdays();
+
+
+            }
+
+        }
+        static void LoginUser()
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                login = true;
+            }
+            else
+            {
+                Console.WriteLine("No user registered.");
+            }
+        }
+        static void DisplayWelcome()
+        {
+            Console.Clear();
+            Console.WriteLine("WELCOME" + username);
+            using (StreamWriter writer = File.AppendText(contentFilePath))
+            {
+                writer.WriteLine($"{username},{password}");
+            }
+            while (login)
+            {
+                Console.WriteLine("Press 1 : LOGOUT.");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    login = false;
+                    Console.WriteLine("LOGGED OUT");
+                    
+                    DisplayWelcome();
+
+                }
+
+            }
+
+        }
+        static void RegisterUser()
+        {
+            username = GenerateEmail();
+            password = GeneratePassword();
+            Console.WriteLine("new user created with email" + username);
+        }
+
+
+        static void LogInAgain()
+        {
+            Thread loginagain = new Thread(LoginUser);
+            loginagain.Start();
+            loginagain.Join();
+
+            if (login)
+            {
+                DisplayWelcome();
+            }
+        }
+        static void CreateNewUser()
+        {
+            username = GenerateEmail();
+            password = GeneratePassword();
+            Console.WriteLine("new user created with email" + username);
+            login = true;
+            DisplayWelcome();
+        }
+        static Random rand = new Random();
+        static int emailCounter = 1;
+
+        static string GenerateEmail()
+        {
+            string email = "jiya" + emailCounter.ToString("D2") + "@gmail.com";
+            emailCounter++;
+            return email;
+        }
+
+
+        static string GeneratePassword()
+        {
+            string allowedChars = "0123456789";
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                password.Append(allowedChars[random.Next(allowedChars.Length)]);
+            }
+            return password.ToString();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programs.Options();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 24 june 2024/24 june 2024/birthdays.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+namespace MyProgram
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public partial class Programs
+    {
+        public static void Birthdays()
+        {
+            Console.Write("Enter the filename of BIRTHDAYS: ");
+            string filename = Console.ReadLine();
+            List<User> users = ReadUsersFromFile(filename);
+            if (users != null)
+            {
+                List<User> upcomingBirthdays = FindUpcomingBirthdays(users, 10);
+
+                if (upcomingBirthdays.Count > 0)
+                {
+                    Console.WriteLine("Birthdays within the next 10 days are listed:");
+                    foreach (var user in upcomingBirthdays)
+                    {
+                        Console.WriteLine($"{user.Name}: {user.DateOfBirth.ToString("dd-MM-yyyy")}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No upcoming birthdays within the next 10 days.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error!! Check your filename again.");
+            }
+        }
+
+        static List<User> ReadUsersFromFile(string filename)
+        {
+            List<User> users = new List<User>();
+            try
+            {
+                string[] lines = File.ReadAllLines(filename);
+                foreach (var line in lines)
+                {
+                    string[] parts = line.Split(',');
+                    if (parts.Length >= 2)
+                    {
+                        string name = parts[0];
+                        DateTime dob;
+                        if (DateTime.TryParseExact(parts[1], "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dob))
+                        {
+                            users.Add(new User { Name = name, DateOfBirth = dob });
+                        }
+                    }
+                }
+                return users;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
+        }
+
+        static List<User> FindUpcomingBirthdays(List<User> users, int days)
+        {
+            DateTime today = DateTime.Today;
+            DateTime upcomingDateLimit = today.AddDays(days);
+            List<User> upcomingBirthdays = new List<User>();
+            foreach (var user in users)
+            {
+                DateTime nextBirthday = new DateTime(today.Year, user.DateOfBirth.Month, user.DateOfBirth.Day);
+                if (nextBirthday < today)
+                {
+                    nextBirthday = nextBirthday.AddYears(1);
+                }
+                if (nextBirthday <= upcomingDateLimit)
+                {
+                    upcomingBirthdays.Add(user);
+                }
+            }
+            return upcomingBirthdays;
+        }
+    }
+
+    
+}
+    
